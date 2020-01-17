9418b30ef1836aab56950c759826638237135cfe
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-17 12:30:13 -0800

    add methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,5 +19,34 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
 
 end
