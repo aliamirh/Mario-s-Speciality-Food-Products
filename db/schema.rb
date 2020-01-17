b3ce6f5d6b440d6996857e561c8d4ec6bdd59393
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-17 10:43:32 -0800

    add testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_180130) do
+ActiveRecord::Schema.define(version: 2020_01_17_182236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_01_17_180130) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
