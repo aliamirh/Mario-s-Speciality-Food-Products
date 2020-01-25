478b704647fc1d408faac4e8dec1918524398b85
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-25 14:10:38 -0800

    add some integration testing

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 describe "product page" do
-  it "verifies the product page" do
-     visit('/')
-   expect(page).to have_content 'No new products to show.'
-   expect(page).to have_content 'Products'
-   end
- end
 
-describe "describe review page" do
-  it "verifies the reviews page" do
-    visit product_review_path
-    expect(page).to have_content 'Reviewer:'
+  it "verifies the product page" do
+    visit('/')
+    expect(page).to have_content 'No new products to show.'
+    expect(page).to have_content 'Products'
+  end
+
+  it "click on sign up" do
+    visit('/')
+    click_on "Sign up"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password"
+  end
+
+  it "click on sign in" do
+    visit('/')
+    click_on "Sign in"
   end
 end
