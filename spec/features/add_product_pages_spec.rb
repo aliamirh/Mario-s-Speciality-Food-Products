c120066dcd5fe56807d545a4a7b80b6d3fd30fc7
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-25 15:46:37 -0800

    add more integration testing

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -8,18 +8,31 @@ describe "product page" do
     expect(page).to have_content 'Products'
   end
 
-  it "click on sign up" do
+  it "click on Sign up and create account" do
     visit('/')
     click_on "Sign up"
     expect(page).to have_content "Email"
     expect(page).to have_content "Password"
+    fill_in "user[email]", :with => 'test11@test11.com'
+    fill_in "Password", :with => '123'
+    fill_in "Password confirmation", :with => '123'
+    check "user[admin]"
+    click_on "Sign up"
+    visit '/'
+    click_on "Create new product"
   end
 
-  it "click on sign in" do
+  it "click on sign in and redirect back to sign in page" do
     visit('/')
     click_on "Sign in"
     fill_in "Email", :with => 'test1@test1.com'
     fill_in "Password", :with => '123'
     expect(page).to have_content "Sign in"
+  end
+
+  it "should click on Create new product link and redirect back to root page" do
+    visit('/')
+    click_on "Create new product"
+    expect(page).to have_content "Products"
   end
 end
